871042eb2871b87fd8ee87c9daae5810a1db9570
Sobral <robson@oficiodesign.com>
2023-09-27 10:03:37 -0300

    Install initial styles

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  css: ['@/assets/scss/index.scss'],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/assets/scss/abstracts" as my;',
+        },
+      },
+    },
+  },
   typescript: {
     strict: true,
     typeCheck: true,
