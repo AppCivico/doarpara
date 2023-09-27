404efca9a8a721d84a5de816e79bd2e753707cd4
Sobral <robson@oficiodesign.com>
2023-09-27 09:32:38 -0300

    Setup code linting and enable TS

---
+++ .eslintrc.cjs
@@ -0,0 +1,29 @@
+require('@rushstack/eslint-patch/modern-module-resolution');
+
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  globals: {
+    defineNuxtConfig: 'readonly',
+    vue: 'readonly',
+  },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+  },
+  extends: [
+    '@nuxt/eslint-config',
+    '@nuxtjs/eslint-config-typescript',
+    'plugin:vue/vue3-recommended',
+    '@vue/eslint-config-airbnb',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+  },
+};
