4cbe8bee3101a6fb0d96ce9c78693509c9c79283
Sobral <robson@oficiodesign.com>
2023-10-09 21:50:33 -0300

    Add social networks component

---
+++ components/socialNetworks.vue
@@ -0,0 +1,44 @@
+<template>
+  <ul class="social-networks">
+    <li class="social-networks__item">
+      <a
+        class="social-networks__link social-networks__link--linkedin"
+        href="https://linkedin.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        LinkedIn
+      </a>
+    </li>
+    <li class="social-networks__item">
+      <a
+        class="social-networks__link social-networks__link--twitter"
+        href="https://twitter.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Twitter
+      </a>
+    </li>
+    <li class="social-networks__item">
+      <a
+        class="social-networks__link social-networks__link--instagram"
+        href="https://instagram.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Instagram
+      </a>
+    </li>
+    <li class="social-networks__item">
+      <a
+        class="social-networks__link social-networks__link--facebook"
+        href="https://facebook.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Facebook
+      </a>
+    </li>
+  </ul>
+</template>
