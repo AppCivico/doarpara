dc87db2a754b5638391bf9eded838643e55a5ad3
Sobral <robson@oficiodesign.com>
2023-10-14 17:13:39 -0300

    Set layout of tabs

--- app.vue
+++ app.vue
@@ -47,143 +47,39 @@
             aria-label="Seções dessa campanha"
           >
             <ul class="tab-navigation__list">
-              <li class="tab-navigation__item">
+              <li
+                v-for="tab in tabs"
+                :key="tab.id"
+                class="tab-navigation__item"
+              >
                 <a
                   class="tab-navigation__link"
-                  href="#campanha"
-                  aria-controls="campanha"
+                  :href="`#${tab.id}`"
+                  :aria-controls="`#${tab.id}`"
                   role="tab"
+                  @click.prevent="changeTab($event)"
                 >
-                  {{ $t('nav.campaign') }}
-                </a>
-              </li>
-              <li class="tab-navigation__item">
-                <a
-                  class="tab-navigation__link"
-                  href="#transparencia"
-                  aria-controls="transparencia"
-                  role="tab"
-                >
-                  {{ $t('nav.transparency') }}
-                </a>
-              </li>
-              <li class="tab-navigation__item">
-                <a
-                  class="tab-navigation__link"
-                  href="#testemunhos"
-                  aria-controls="testemunhos"
-                  role="tab"
-                >
-                  {{ $t('nav.testimony', 0) }}
-                </a>
-              </li>
-              <li class="tab-navigation__item">
-                <a
-                  class="tab-navigation__link"
-                  href="#faq"
-                  aria-controls="testemunhos"
-                  role="tab"
-                >
-                  <abbr title="Perguntas Frequentes">FAQ</abbr>
+                  {{ tab.title }}
                 </a>
               </li>
             </ul>
           </nav>
 
-          <div class="text-body__tabs-and-rewards">
+          <div class="text-body__tabs-and-rewards" role="tabpanel">
             <section
               aria-live="polite"
               role="region"
               class="tab-list text-body__tab-list"
             >
-              <article id="campanha" role="tabpanel" class="tab-list__item">
-                <h2>
-                  Vamos falar sobre crianças vulneráveis e como VOCÊ pode ajudar
-                  crianças em idade escolar presas nela...
-                </h2>
-
-                <p>
-                  Imagine não poder pagar o básico para se manter limpo; isso é
-                  pobreza higiénica e tem um efeito doloroso nos nossos filhos.
-                  Na verdade, 66% dos professores afirmam ter testemunhado o
-                  bullying de uma criança devido ao fraco acesso à higiene. Como
-                  somos uma marca de produtos de limpeza, sabíamos que
-                  poderíamos fazer algo para ajudar… então iniciamos o Projeto
-                  Espuma nas Escolas.
-                </p>
-
-                <p>
-                  A idéia é criar mini-lavandarias gratuitas nas escolas de todo
-                  o Brasil, fornecendo uma máquina de lavar/secar roupa e todo o
-                  detergente necessário para garantir que nenhuma criança sob os
-                  seus cuidados sofra a vergonha, o bullying e a exclusão de não
-                  ter roupas e uniformes limpos. Todos merecem sentir-se limpos.
-                  A pobreza higiénica afecta mais de 3 milhões de nós aqui no
-                  Brasil, mas passa despercebida porque existe um estigma em não
-                  conseguirmos manter-nos limpos e, como nação, simplesmente não
-                  discutimos isso.
-                </p>
-
-                <p>
-                  Tudo isto resulta num impacto extremamente negativo na saúde
-                  mental das crianças – mais de 72% sofrem de baixa auto-estima,
-                  questionam por que não podem convidar os amigos e o seu
-                  desempenho escolar é prejudicado. Oito em cada dez professores
-                  do ensino primário afirmam ter testemunhado um aumento no
-                  número de crianças que frequentam a escola sem lavar ou usar
-                  uniformes sujos nos últimos 5 anos e têm intervindo a um ritmo
-                  crescente (um terço dos professores compra regularmente
-                  produtos de higiene essenciais para os seus alunos).
-                </p>
-
-                <h2>Ações que pretendemos colocar em prática</h2>
-                <p>
-                  A idéia é criar mini-lavandarias gratuitas nas escolas de todo
-                  o Brasil, fornecendo uma máquina de lavar/secar roupa e todo o
-                  detergente necessário para garantir que nenhuma criança sob os
-                  seus cuidados sofra a vergonha, o bullying e a exclusão de não
-                  ter roupas e uniformes limpos. Todos merecem sentir-se limpos.
-                  A pobreza higiénica afecta mais de 3 milhões de nós aqui no
-                  Brasil, mas passa despercebida porque existe um estigma em não
-                  conseguirmos manter-nos limpos e, como nação, simplesmente não
-                  discutimos isso.
-                </p>
-
-                <p>
-                  Tudo isto resulta num impacto extremamente negativo na saúde
-                  mental das crianças – mais de 72% sofrem de baixa auto-estima,
-                  questionam por que não podem convidar os amigos e o seu
-                  desempenho escolar é prejudicado. Oito em cada dez professores
-                  do ensino primário afirmam testemunhado um aumento no número
-                  de crianças que frequentam a escola sem lavar ou usar
-                  uniformes sujos nos últimos 5 anos e têm intervindo a um ritmo
-                  crescente (um terço dos professores compra regularmente
-                  produtos de higiene essenciais para os seus alunos).
-                </p>
-
-                <p>
-                  Imagine não poder pagar o básico para se manter limpo; isso é
-                  pobreza higiénica e tem um efeito doloroso nos nossos filhos.
-                  Na verdade, 66% dos professores afirmam ter testemunhado o
-                  bullying de uma criança devido ao fraco acesso à higiene. Como
-                  somos uma marca de produtos de limpeza, sabíamos que
-                  poderíamos fazer algo para ajudar… então iniciamos o Projeto
-                  Espuma nas Escolas.
-                </p>
-              </article>
               <article
-                id="transparencia"
-                role="tabpanel"
+                v-for="tab in tabs"
+                :id="tab.id"
+                :key="tab.id"
+                role="tab"
                 class="tab-list__item"
-              >
-                <p>If you click this tab then your life will be better</p>
-              </article>
-              <article id="testemunhos" role="tabpanel" class="tab-list__item">
-                <p>Know your roles</p>
-              </article>
-              <article id="faq" role="tabpanel" class="tab-list__item">
-                <p>Know your roles</p>
-              </article>
+                :aria-selected="tab.id === currentTab"
+                v-html="tab.content"
+              ></article>
 
               <footer class="text-body__call-to-faq">
                 <p>
@@ -270,6 +166,31 @@
     </footer>
   </div>
 </template>
-<script setup>
-import rewards from '@/mocks/rewards.js';
+<script setup lang="ts">
+import rewards from '../mocks/rewards.js';
+import tabs from '../mocks/tabs.js';
+
+const currentTab = ref('');
+
+currentTab.value = tabs[0]?.id;
+
+function changeTab(event: Event): void {
+  const { target } = event as MouseEvent;
+
+  if (target instanceof HTMLElement && target.hasAttribute('href')) {
+    const href = target.getAttribute('href');
+
+    if (href) {
+      const hash = href.split('#')[1];
+
+      if (hash) {
+        event.preventDefault();
+        currentTab.value = hash;
+        if (window.history) {
+          window.history.pushState({}, '', href);
+        }
+      }
+    }
+  }
+}
 </script>
