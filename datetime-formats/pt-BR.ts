9a159de6dc3df040c25441ec5f809bb4772b53b1
Sobral <robson@oficiodesign.com>
2023-11-04 18:57:32 -0300

    Add medium date format

--- datetime-formats/pt-BR.ts
+++ datetime-formats/pt-BR.ts
@@ -7,6 +7,13 @@ export default {
     weekday: 'long',
     year: 'numeric',
   },
+  medium: {
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    month: 'numeric',
+    year: 'numeric',
+  },
   short: {
     day: 'numeric',
     month: 'numeric',
