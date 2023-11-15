6a7ff195d5fd1a9597ad6f626dd29121bc6365b1
Sobral <robson@oficiodesign.com>
2023-11-15 11:27:16 -0300

    Fix taxes schema

--- data/taxes.ts
+++ data/taxes.ts
@@ -1,31 +1,31 @@
 type Taxes = {
   [key: string]: {
-    [key: string]: {
-      [key: string]: string | number
-    }
+    label: string;
+    text: string;
+    percent: number;
+    tax: number;
+    [key: string]: string | number
   };
 };
 
 const taxes: Taxes = {
-  taxes: {
-    credit_card: {
-      label: 'cartão de crédito',
-      text: '5,5%',
-      percent: 5.5,
-      tax: 0,
-    },
-    boleto: {
-      label: 'boleto pago',
-      text: '3,5% + R$ 3,90',
-      percent: 3.5,
-      tax: 390,
-    },
-    pix: {
-      label: 'PIX',
-      text: '2,99%',
-      percent: 2.99,
-      tax: 0,
-    },
+  credit_card: {
+    label: 'cartão de crédito',
+    text: '5,5%',
+    percent: 5.5,
+    tax: 0,
+  },
+  boleto: {
+    label: 'boleto pago',
+    text: '3,5% + R$ 3,90',
+    percent: 3.5,
+    tax: 390,
+  },
+  pix: {
+    label: 'PIX',
+    text: '2,99%',
+    percent: 2.99,
+    tax: 0,
   },
 };
 
