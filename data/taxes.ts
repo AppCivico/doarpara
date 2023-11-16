2199d0601fe0a88c44961e3aaa543b61cddfdc07
Sobral <robson@oficiodesign.com>
2023-11-16 14:36:40 -0300

    Update taxes sample

--- data/taxes.ts
+++ data/taxes.ts
@@ -1,7 +1,5 @@
 type Taxes = {
   [key: string]: {
-    label: string;
-    text: string;
     percent: number;
     tax: number;
     [key: string]: string | number
@@ -10,20 +8,14 @@ type Taxes = {
 
 const taxes: Taxes = {
   credit_card: {
-    label: 'cartão de crédito',
-    text: '5,5%',
     percent: 5.5,
     tax: 0,
   },
   boleto: {
-    label: 'boleto pago',
-    text: '3,5% + R$ 3,90',
     percent: 3.5,
     tax: 390,
   },
   pix: {
-    label: 'PIX',
-    text: '2,99%',
     percent: 2.99,
     tax: 0,
   },
