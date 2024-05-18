a8806616e667884f4cf8482493141719e4c39e31
Sobral <robson@oficiodesign.com>
2024-05-18 00:01:16 -0300

    fix: List of goals wrong when there's an achieved one

--- utils/getCurrentGoal.ts
+++ utils/getCurrentGoal.ts
@@ -1,8 +1,12 @@
 import type { Goal } from '@/doar-para';
 
-export default (goals:Goal[], totalAmount:number = 0) => (
-  goals.find((x: Goal) => x.amount > totalAmount)
-   || goals[goals.length - 1]
-)?.amount
+export default (goals:Goal[], totalAmount:number = 0) => {
+  const sortedGoals = [...goals].sort((a, b) => a.amount - b.amount);
+
+  return (
+    sortedGoals.find((x: Goal) => x.amount > totalAmount)
+    || sortedGoals[sortedGoals.length - 1]
+  )?.amount
   || totalAmount
   || 0;
+};
