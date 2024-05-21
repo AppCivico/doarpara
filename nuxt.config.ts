f297eeb687c0c014e4df54b88bb16e51a5c39a60
ingredbarraqui <ingred.vferreira@gmail.com>
2024-05-21 18:29:50 -0300

    feat: sentry

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -38,6 +38,10 @@ export default defineNuxtConfig({
     'nuxt-gtag',
     '@nuxtjs/sentry',
   ],
+  sentry: {
+    dsn: 'https://aeb86c65c0f8bc43f849b547b2d62db7@o75154.ingest.us.sentry.io/4507295736594432', // Enter your project's DSN.
+    // Additional module options go here.
+  },
   runtimeConfig: {
     public: {
       title: process.env.APP_TITLE
