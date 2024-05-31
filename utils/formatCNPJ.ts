36e9fd225403431ac8a010f5fff284c83df862c0
Sobral <robson@oficiodesign.com>
2024-05-31 11:05:40 -0300

    fix: Prevent issue on formatting undefined person documents

--- utils/formatCNPJ.ts
+++ utils/formatCNPJ.ts
@@ -1,3 +1,3 @@
-export default (value:string) => (/^\d{14}$/.test(value.trim())
+export default (value:string = '') => (/^\d{14}$/.test(value.trim())
   ? value.trim().replace(/[^0-9]/g, '').replace(/^(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})$/g, '$1.$2.$3/$4-$5')
   : value);
