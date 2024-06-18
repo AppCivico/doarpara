d3d8040124b0c777490332f0ca74405bba7b69d4
Sobral <robson@oficiodesign.com>
2024-06-17 22:14:39 -0300

    feat: Enable Meta pixel tracking code

--- pages/[campaignSlug].vue
+++ pages/[campaignSlug].vue
@@ -69,7 +69,28 @@ if (campaign.value) {
     htmlAttrs: {
       class: () => normalizeThemeName(campaign.value?.theme),
     },
+    script: [] as Array<Record<string, any>>,
+    noscript: [] as Array<Record<string, any>>,
   };
+
+  if (campaign.value.facebook_pixel) {
+    meta.script.push({
+      'textContent': `!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '${campaign.value.facebook_pixel}');
+fbq('track', 'PageView');`,
+    });
+    meta.noscript.push({
+      'textContent': `<img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=${campaign.value.facebook_pixel}&ev=PageView&noscript=1"/>`,
+    });
+  }
 
   useHead(meta);
 
