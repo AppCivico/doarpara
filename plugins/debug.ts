86bd9afc8406e75932dc1718c3bc96bf5af9185a
Sobral <robson@oficiodesign.com>
2024-06-20 14:55:51 -0300

    fix: change debugger shortcut

--- plugins/debug.ts
+++ plugins/debug.ts
@@ -1,9 +1,12 @@
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.directive('debug', {
     beforeMount: (el, binding) => {
+      const primaryKey = 'Control';
+      const secondaryKey = 'CapsLock';
+
       el.classList.add('debug');
       el.setAttribute('hidden', '');
-      let wasShiftPressed = false;
+      let isSecondaryKeyPressed = false;
 
       if (binding.value) {
         el.setAttribute('data-debug', binding.value);
@@ -13,23 +16,23 @@ export default defineNuxtPlugin((nuxtApp) => {
           return; // Do nothing if the event was already processed
         }
 
-        if (event.getModifierState('Control')) {
-          if (event.key === 'Shift') {
-            if (wasShiftPressed) {
+        if (event.getModifierState && event.getModifierState(primaryKey)) {
+          if (event.key === secondaryKey) {
+            if (isSecondaryKeyPressed) {
               if (el.hasAttribute('hidden')) {
                 el.removeAttribute('hidden');
               } else {
                 el.setAttribute('hidden', '');
               }
-              wasShiftPressed = false;
+              isSecondaryKeyPressed = false;
             } else {
-              wasShiftPressed = true;
+              isSecondaryKeyPressed = true;
               setTimeout(() => {
-                wasShiftPressed = false;
+                isSecondaryKeyPressed = false;
               }, 300);
             }
-          } else if (wasShiftPressed) {
-            wasShiftPressed = false;
+          } else if (isSecondaryKeyPressed) {
+            isSecondaryKeyPressed = false;
           }
         }
       });
