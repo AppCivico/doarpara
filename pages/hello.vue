94d9f8c8c4ce765f65efe813b8b560f3294f5fb2
Sobral <robson@oficiodesign.com>
2025-12-18 09:12:51 -0300

    test: Add hello world route

---
+++ pages/hello.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Hello, world!</h1>
+    <p>Request time: {{ requestTime }}</p>
+  </div>
+</template>
+<script setup lang="ts">
+definePageMeta({
+  layout: 'minimal',
+});
+
+const requestTime = new Date().toISOString();
+</script>
